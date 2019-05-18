0d1f5caad0a15f1d99042a08ea051a9fc0a5c1d6
Monica Mantilla <monicamantilla@macbook-pro-2.home>
2019-05-18 18:26:25 -0400

    require

--- liri.js
+++ liri.js
@@ -1,1 +1,17 @@
 require("dotenv").config();
+
+const axios = require("axios")
+const Spotify = require()
+const keys= require()
+const moment =  require()
+const fs = require("fs");
+
+var query = process.argv[2];
+
+const spotify = new Spotify(keys.spotify);
+switch(option){
+    case "movie-this"
+
+}
+
+fs.readFile("random.txt)
